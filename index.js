d72651aa62e5018a9e2180ca0aa4c1019a0630b1
Justin Taylor <justin2taylor@gmail.com>
2022-04-01 14:18:10 -0700

    init

---
+++ index.js
@@ -0,0 +1,78 @@
+#!/usr/bin/env node
+
+// @ts-check
+"use strict";
+
+const fs = require("fs-extra");
+const path = require("path");
+
+const yargs = require("yargs/yargs");
+const { hideBin } = require("yargs/helpers");
+const argv = yargs(hideBin(process.argv)).argv;
+
+const frameworks = [
+  { name: "react", pretty: "React" },
+  { name: "vue", pretty: "Vue" },
+  { name: "svelte", pretty: "Svelte" },
+];
+
+const init = async () => {
+  const dir = process.cwd();
+  const templateStr = argv["template"];
+  const name = argv["_"].pop();
+
+  const template = frameworks.find((f) => f.name === templateStr);
+
+  if (frameworks.map((f) => f.name).includes(templateStr) && name?.length > 0) {
+    const dest = path.join(dir, name);
+    let bolt = path.join(__dirname, "node_modules", "bolt-cep");
+    const isSymlink = fs.lstatSync(bolt).isSymbolicLink();
+    bolt = isSymlink ? fs.realpathSync(bolt) : bolt;
+    fs.mkdirSync(dest);
+    const ignoreItems = [".git", "node_modules", "dist"];
+    fs.readdirSync(bolt).map((item) => {
+      if (!ignoreItems.includes(item)) {
+        const srcItem = path.join(bolt, item);
+        const dstItem = path.join(dest, item);
+        fs.copySync(srcItem, dstItem);
+      }
+    });
+    const jsFolder = path.join(dest, "src", "js");
+
+    // Remove Placeholder
+    const tempMain = path.join(jsFolder, "main");
+    if (fs.existsSync(tempMain)) {
+      fs.rmSync(tempMain, { recursive: true });
+    } else {
+      console.error("TEMP MAIN doesn't exist");
+    }
+
+    // Move Template
+    const templateFolder = path.join(jsFolder, `template-${template.name}`);
+    const newName = path.join(jsFolder, `main`);
+    if (fs.existsSync(templateFolder)) {
+      fs.renameSync(templateFolder, newName);
+    }
+
+    // Delete Extra Templates
+    fs.readdirSync(jsFolder).map((extraTemplate) => {
+      if (extraTemplate.indexOf("template-") === 0) {
+        fs.rmSync(path.join(jsFolder, extraTemplate), { recursive: true });
+      }
+    });
+
+    console.log(`New Bolt CEP generated with ${template.pretty}: ${name}`);
+    console.info("--------------");
+    console.log(`Path :: ${dest}`);
+  } else {
+    console.error("Command Incorrect");
+    console.info("");
+    console.info("EXAMPLES:");
+    console.info("--------------");
+    console.info(`(e.g. "yarn create bolt-cep my-app --template react")`);
+    console.info(`(e.g. "yarn create bolt-cep my-cool-app --template vue")`);
+    console.info("--------------");
+  }
+};
+
+init();
